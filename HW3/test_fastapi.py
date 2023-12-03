1111324be35cff8e3506f7477e1903fb02efaf4a
stud <vad89@inbox.ru>
2023-12-03 16:57:21 +0300

    Add HW3: test_fastapi.py, requirements.txt, screenshot.png. Edit README.md for HW3

---
+++ HW3/test_fastapi.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    context: str
+    question: str
+
+app = FastAPI()
+
+model = pipeline('question-answering', 'timpal0l/mdeberta-v3-base-squad2')
+
+
+@app.get('/')
+def root():
+    return {'message': "It's a live (by group 1.7)"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """
+    API позволяет найти ответ на заданный вопрос в тексте.
+    Для работы необходимо сделать POST запрос со следуюими параметрами в теле:
+    context - строка, содержащая текст
+    queston - строка, содержащая вопрос, ответ на который необходимо найти.
+
+    За реализацию функционала отвечает модель timpal0l/mdeberta-v3-base-squad2
+    с сайта huggingface.co. Модель поддерживает ru, en и множество других языков.
+    """
+    return model(question = item.question, context = item.context)

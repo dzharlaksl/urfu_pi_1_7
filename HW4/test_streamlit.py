8ffa7c865560a478aa7ccd5f4973da54e3899ee6
stud <vad89@inbox.ru>
2023-12-06 09:38:06 +0300

    add directory HW4 with script for cloud run

---
+++ HW4/test_streamlit.py
@@ -0,0 +1,45 @@
+from transformers import pipeline
+import streamlit as st
+
+
+@st.cache_resource
+def load_model():
+    model = pipeline('question-answering', 'timpal0l/mdeberta-v3-base-squad2')
+    return model
+
+
+# Markdown не применяется форматирование жирным шрифтом, если с краю стоит "." или ",". 
+# Модель ошибочно может выводить их в ответ, поэтому такие символы надо удалить.
+def strip_symbol(text, symbols):
+
+    # Проходим текст 2 раза: с конца строки и с начала
+    for step in range(2):
+
+        text = text[::-1]
+        start = 0
+        for i in text:
+            if i in symbols:
+                start += 1
+            else:
+                break
+
+        text = text[start:]
+
+    return text
+
+
+model = load_model()
+
+st.title('Поиск ответа на вопрос в тексте')
+
+context = st.text_input('Введите текст, в котором необходимо искать ответы:', value='My name is Tim and I live in Sweden.')
+
+question = st.text_input('Введите вопрос:', value='Where do I live?')
+
+result = st.button('Получить ответ')
+
+if result:
+    response = model(question = question, context = context)
+    print(response)
+    text = 'Ответ **' + strip_symbol(response['answer'], ' .<>:,=*') + '** с вероятностью **' + str(response['score']) + '**.'
+    st.write(text)
